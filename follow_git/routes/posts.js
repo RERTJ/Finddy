f6d2db4482af7ec435e1a163e5e1747a3d33ff6b
Oliver <Oliver_Husky@Olivers-MacBook-Air.local>
2017-03-13 00:41:51 +0800

    3.13 a know nothing, hungry night

---
+++ follow_git/routes/posts.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+var checkLogin = require('../middlewares/check').checkLogin;
+
+
+
+router.get('/', function(req, res, next) {
+  res.send(req.flash());
+});
+
+//Post an activity
+router.post('/', checkLogin, function(req, res, next) {
+  res.send(req.flash());
+});
+router.get('/create', checkLogin, function(req, res, next) {
+  res.send(req.flash());
+});
+
+//Certain activity
+router.get('/:postId', function(req, res, next) {
+  res.send(req.flash());
+});
+//Edit Certain activity
+router.get('/:postId/edit', checkLogin, function(req, res, next) {
+  res.send(req.flash());
+});
+router.post('/:postId/edit', checkLogin, function(req, res, next) {
+  res.send(req.flash());
+});
+
+// POST /posts/:postId/comment 创建一条留言
+router.post('/:postId/comment', checkLogin, function(req, res, next) {
+  res.send(req.flash());
+});
+
+module.exports = router;
