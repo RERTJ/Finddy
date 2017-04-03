0677b8c1aed88e373ee7818260a3ff50a5693b00
chengnn119 <115046999@link.cuhk.edu.hk>
2017-04-03 17:06:59 +0800

    Confirm password in register page using jquery
    Added function in custom.js

--- follow_git/public/js/custom.js
+++ follow_git/public/js/custom.js
@@ -1,1 +1,18 @@
 /* Write here your custom javascript codes */
+
+/*confirm password*/
+jQuery(document).ready(function() {
+        $('#register').click(function(){
+            var pass = $('#password').val();
+            var pass2 = $('#repassword').val();
+            if (pass == '')
+                alert('Please enter a password');
+            else if (pass2 == '')
+                alert('Please re-enter the password');
+            else if (pass != pass2)
+                alert('The passwords do not match!');
+            else
+            	window.location.href = "login.html";
+
+        });   
+    });
