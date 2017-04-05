eaf371491f2d8b8d3a01099df18fdbbaa35342af
chengnn119 <115046999@link.cuhk.edu.hk>
2017-04-05 22:38:35 +0800

    Write a function to verify phone number
    Restricted to Hong Kong local numbers in the form of XXXXXXXX (8-digits)
    and the first digit can't be 0

--- follow_git/public/js/custom.js
+++ follow_git/public/js/custom.js
@@ -1,6 +1,6 @@
 /* Write here your custom javascript codes */
 
-/*confirm password*/
+/*confirm password in registration page*/
 jQuery(document).ready(function() {
         $('#register').click(function(){
             var pass = $('#password').val();
@@ -16,3 +16,62 @@ jQuery(document).ready(function() {
 
         });   
     });
+
+/*confirm phone number in manageProfile page*/
+jQuery(document).ready(function(){
+	$('.save').click(function(){
+		var mobile = $('#mobile').val();
+		if (isNaN(mobile) || Number(mobile) < 10000000 || Number(mobile) > 99999999)
+			alert('Please enter a valid mobile phone number according to example!');
+	});	
+});
+
+/*function for google map API*/
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 22.093275, lng: 114.301758},
+    zoom: 10
+  });
+
+  var input = document.getElementById('pac-input');
+
+  var autocomplete = new google.maps.places.Autocomplete(input);
+  autocomplete.bindTo('bounds', map);
+
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  var infowindow = new google.maps.InfoWindow();
+  var marker = new google.maps.Marker({
+    map: map
+  });
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+
+  autocomplete.addListener('place_changed', function() {
+    infowindow.close();
+    var place = autocomplete.getPlace();
+    if (!place.geometry) {
+      return;
+    }
+
+    if (place.geometry.viewport) {
+      map.fitBounds(place.geometry.viewport);
+    } else {
+      map.setCenter(place.geometry.location);
+      map.setZoom(17);
+    }
+
+    // Set the position of the marker using the place ID and location.
+    marker.setPlace({
+      placeId: place.place_id,
+      location: place.geometry.location
+    });
+    marker.setVisible(true);
+
+    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+        'Place ID: ' + place.place_id + '<br>' +
+        place.formatted_address);
+    infowindow.open(map, marker);
+  });
+}
